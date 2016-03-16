7459763e2df7c6470874973e7d32f9eea9a00cb1
Radu Achim <radu.achim@iteam.se>
2016-03-16 15:54:34 +0100

    testing a new ui

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-const kue = require('kue')
+// const kue = require('kue')
 
+// kue.createQueue({
+//   prefix: process.env.KUE_PREFIX,
+//   redis: process.env.REDIS_URL || 'redis://redis:6379'
+// })
+
+// kue.app.listen(5000)
+
+const kue = require('kue')
+const express = require('express')
+const ui = require('kue-ui')
+const app = express()
+
+// connect kue to appropriate redis, or omit for default localhost
 kue.createQueue({
-  prefix: process.env.KUE_PREFIX,
-  redis: process.env.REDIS_URL || 'redis://redis:6379'
+  redis: process.env.REDIS_URL || 'redis://docker:6379'
 })
 
-kue.app.listen(5000)
+ui.setup({
+  apiURL: '/api', // IMPORTANT: specify the api url
+  baseURL: '/', // IMPORTANT: specify the base url
+  updateInterval: 5000 // Optional: Fetches new data every 5000 ms
+})
+
+// Mount kue JSON api
+app.use('/api', kue.app)
+// Mount UI
+app.use('/', ui.app)
+
+app.listen(3000, (err, data) => {
+  if (err) {
+    console.log('error', err)
+  }
+  console.log('listening on port', 3000)
+})
